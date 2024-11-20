e43e811c0f4267f4c3c8a14a29c964bce7bb3ce9
Joshua Carmack <joshuahcarmack@gmail.com>
2024-11-20 08:29:34 -0500

    Updated width

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
             border-radius: 10px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
             text-align: center;
-            max-width: 400px;
+            max-width: 75%;
             width: 100%;
         }
 
